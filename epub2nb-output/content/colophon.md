802e136c8e03a5c85e75944747feb5f29db43216
tesarj <102167704+tesarj@users.noreply.github.com>
2022-04-30 10:17:18 +0000

    Deploying to docs from @ books-are-next/keller-az-na-dno-blahobytu@451f68e54f41033bad1219e37ecdb90b6bb3025a 🚀

--- epub2nb-output/content/colophon.md
+++ epub2nb-output/content/colophon.md
@@ -2,3 +2,69 @@
 title: Tiráž
 ---
 
+***
+
+Až na dno blahobytu
+
+Ke společenským kořenům ekologické krize
+
+Jan Keller
+
+
+***
+
+Znění tohoto textu vychází z díla [Až na dno blahobytu](https://search.mlp.cz/cz/titul/az-na-dno-blahobytu/2000051/#book-content) tak, jak bylo vydáno nakladatelstvím Hnutí Duha v Brně v roce 1995. Pro potřeby vydání Městské knihovny v Praze byl text redakčně zpracován.
+
+![](../Images/MZK_logo_tyrkys_transparent.jpg)
+
+Podle předlohy z fondu Moravské zemské knihovny v Brně.
+
+§
+
+Text díla (Jan Keller: Až na dno blahobytu), publikovaného [Městskou knihovnou v Praze](https://www.mlp.cz/cz/), je vázán autorskými právy a jeho použití je definováno [Autorským zákonem](https://www.mkcr.cz/predpisy-zakonu-709.html) č. 121/2000 Sb.
+
+![](../Images/image001.jpg)
+
+Vydání (obálka, upoutávka, citační stránka a grafická úprava), jehož autorem je Městská knihovna v Praze, podléhá licenci [Creative Commons Uveďte autora-Nevyužívejte dílo komerčně-Zachovejte licenci 3.0 Česko](https://creativecommons.org/licenses/by-nc-sa/3.0/cz/).
+
+  
+
+Verze 1.0 z 1. 4. 2022.
+
+![](../Images/image002.jpg)
+
+
+***
+
+**Jan Keller  
+Až na dno blahobytu**
+
+Ke společenským kořenům ekologické krize
+
+Ilustrace na obálce Albrecht Dürer, dřevoryt Loď bláznů
+
+  
+
+Technická redakce Petra Kučerová
+
+Vydala Městská knihovna v Praze
+
+  
+
+Mariánské nám. 1, 115 72 Praha 1
+
+V MKP 1. elektronické vydání
+
+  
+
+Verze 1.0 z 1. 4. 2022
+
+ISBN 978-80-274-2286-9 (epub)
+
+  
+
+ISBN 978-80-274-2287-6 (pdf)
+
+  
+
+ISBN 978-80-274-2288-3 (prc)
---
+++ service-worker.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-restricted-globals */
+
+/* global self, caches, fetch */
+
+const CACHE = 'cache-451f68e';
+
+self.addEventListener('install', e => {
+  e.waitUntil(precache()).then(() => self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  self.clients
+    .matchAll({
+      includeUncontrolled: true,
+    })
+    .then(clientList => {
+      const urls = clientList.map(client => client.url);
+      console.log('[ServiceWorker] Matching clients:', urls.join(', '));
+    });
+
+  event.waitUntil(
+    caches
+      .keys()
+      .then(cacheNames =>
+        Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName !== CACHE) {
+              console.log('[ServiceWorker] Deleting old cache:', cacheName);
+              return caches.delete(cacheName);
+            }
+            return null;
+          })
+        )
+      )
+      .then(() => {
+        console.log('[ServiceWorker] Claiming clients for version', CACHE);
+        return self.clients.claim();
+      })
+  );
+});
+
+function precache() {
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./az_na_dno_blahobytu_002.html","./az_na_dno_blahobytu_005.html","./az_na_dno_blahobytu_006.html","./az_na_dno_blahobytu_007.html","./az_na_dno_blahobytu_008.html","./az_na_dno_blahobytu_009.html","./az_na_dno_blahobytu_010.html","./az_na_dno_blahobytu_011.html","./az_na_dno_blahobytu_012.html","./az_na_dno_blahobytu_013.html","./az_na_dno_blahobytu_014.html","./az_na_dno_blahobytu_015.html","./az_na_dno_blahobytu_016.html","./colophon.html","./favicon.png","./index.html","./manifest.json","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image002.jpg","./resources/mzk_logo_tyrkys_transparent.jpg","./resources/obalka_az_na_dno_blahobytu.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
+}
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.open(CACHE).then(cache => {
+      return cache.match(e.request).then(matching => {
+        if (matching) {
+          console.log('[ServiceWorker] Serving file from cache.');
+          console.log(e.request);
+          return matching;
+        }
+
+        return fetch(e.request);
+      });
+    })
+  );
+});
