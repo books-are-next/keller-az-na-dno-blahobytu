7b5944d8070db8ed44c1380a43a1e2439ba29b77
tesarj <102167704+tesarj@users.noreply.github.com>
2022-04-30 11:20:20 +0000

    Deploying to docs from @ books-are-next/keller-az-na-dno-blahobytu@c1d28aec0745bfc0bbd0fa1702aab0013b0da372 🚀

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 /* global self, caches, fetch */
 
-const CACHE = 'cache-451f68e';
+const CACHE = 'cache-c1d28ae';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
